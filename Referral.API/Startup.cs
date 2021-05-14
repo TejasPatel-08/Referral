fcbe3bf0b301587f244354f902c7634cc4156e1f
TejasPatel-08 <77666098+TejasPatel-08@users.noreply.github.com>
2021-05-14 11:45:52 +0530

    code refactor

--- Referral.API/Startup.cs
+++ Referral.API/Startup.cs
@@ -16,35 +16,24 @@ namespace Referral.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
 
             services.AddServiceResolver(Configuration);
 
-            // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
+
             app.UseSwagger();
-
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            //app.UseSwagger(c =>
-            //{
-            //    c.SerializeAsV2 = true;
-            //});
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
+           
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
